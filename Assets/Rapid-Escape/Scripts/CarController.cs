a396ecf351f0ef590f8b406a63ecaaae669a24bc
nishanthkulal <nishanthkulal65462@gmail.com>
2024-10-22 11:20:03 -0700

    Added : wheel movement added

--- Assets/Rapid-Escape/Scripts/CarController.cs
+++ Assets/Rapid-Escape/Scripts/CarController.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class CarController : MonoBehaviour
 {
-
+    [SerializeField] private WheelCollider frontRightWheelColider;
+    [SerializeField] private WheelCollider frontLeftWheelColider;
+    [SerializeField] private WheelCollider backRightWheelColider;
+    [SerializeField] private WheelCollider backLeftWheelColider;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +17,8 @@ public class CarController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        frontRightWheelColider.motorTorque = 10f;
+        frontLeftWheelColider.motorTorque = 10f;
+
     }
 }
